733aea8cb8f0ada5a3077d7e97f74b945eae8a5c
SobuHasy <sobu.hasy@hotmail.com>
2023-11-20 10:07:44 +0100

    Implemented the software, hope for a succesful run

--- src/main/java/h01/GameController.java
+++ src/main/java/h01/GameController.java
@@ -2,7 +2,9 @@
 package h01;
 
 import fopbot.Robot;
+import fopbot.World;
 import h01.template.GameControllerBase;
+import h01.template.Utils;
 import org.tudalgo.algoutils.student.Student;
 
 /**
@@ -26,8 +28,30 @@ public class GameController extends GameControllerBase {
             stopGame();
         }
 
+        // The game is lost if 50% of all fields are dirty
+        int dirtyFields = 0;
+        for (int x = 0; x < World.getWidth(); x++){
+            for (int y = 0; y < World.getHeight(); y++){
+                if (Utils.getCoinAmount(x, y) > 0){
+                    dirtyFields++;
+                }
+            }
+        }
 
-        // If more than 50
+        if (dirtyFields >= (double) World.getWidth() * World.getHeight() / 2){
+            getCleaningRobot().turnOff();
+            System.out.println("Containmants won!");
+            stopGame();
+        }
+
+        // The game is won if the dumping area contains at least 200 coins
+        if (Utils.getCoinAmount(0, World.getHeight() - 1) >= 200){
+            getContaminant1().turnOff();
+            getContaminant2().turnOff();
+            System.out.println("Cleaning robot won!");
+            stopGame();
+        }
+
         Student.crash("H3 - remove if implemented");
     }
 }
